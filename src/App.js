3ae64295d25f4c71dc3c32e22b66f52e265bc7ae
jiyoung <kamo02@daum.net>
2024-09-06 17:28:02 +0900

    react_first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  // logic
+  console.log('App.js');
+
+  const name = '후츠릿';
+  const age = '34';
+
+  const greetingMessage = `안녕하세요. ${name}님, ${age}이시네요
+  환영합니다`;
+  console.log("🚀 ~ App ~ greetingMessage:", greetingMessage)
+
+  const user = {
+    isLoggedIn: true,
+    role:'admin',
+  };
+
+  // 사용자가 로그인 한 상태이면서 역할이 admin인 경우엔 '관리자 페이지 접근 가능', 아니면 '관리지 페이지 접근 불가능;
+
+  if(user.isLoggedIn && user.role === 'admin'){
+    console.log('관리지 페이지 접근 가능')
+
+  } else {
+    console.log('관리지 페이지 접근 불가능')
+  }
+
+  //view
+  return <div>
+    Hello world </div>;
 }
 
 export default App;
